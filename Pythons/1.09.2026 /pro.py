64f5fbe118cc04ff35af2eeb05918480be0b8f30
vasubasri0318-rgb <vasubasri0318@gmail.com>
2026-09-01 23:20:26 +0530

    Create pro.py1

---
+++ Pythons/1.09.2026 /pro.py
@@ -0,0 +1,23 @@
+if(True):
+    print("yes")
+else:
+    print("no")    
+if(False):
+    print("yes")
+else:
+    print("no")    
+print("win"=="win")
+print("win"=="winn")
+mi="win"
+if(mi=="win"):
+    print("Super comeback") 
+else:
+    print("Sirappana alu nee")       
+mi="win"
+if(mi=="lose"):
+    print("Super comeback") 
+else:
+    print("Sirappana alu nee")       
+   
+
+
