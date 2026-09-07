042f6a18bb8f0a1587180a383b1611167857ed4c
vasubasri0318-rgb <vasubasri0318@gmail.com>
2026-09-07 22:03:15 +0530

    Create pro1.py

---
+++ Pythons/1.09.2026 /pro1.py
@@ -0,0 +1,13 @@
+a = int(input("Enter num1: " ))
+b = int(input("Enter num2: " ))
+c = input("Enter operater: ")
+if(c=='+'):
+    print("Addition: ",a+b)
+elif(c=='-'):
+    print("Subtraction: ",a-b)
+elif(c=='*'):
+    print("Subtraction: ",a*b)
+elif(c=='/'):
+    print("Subtraction: ",a/b)
+else:
+    print(" Invalid Input! ")
